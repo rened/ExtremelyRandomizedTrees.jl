008c282f4d0611a5b647a195cd0ce8ee109289d6
Rene Donner <code@donner.at>
2015-01-08 18:45:28 +0100

    regression 2 to 2

--- src/ExtremelyRandomizedTrees.jl
+++ src/ExtremelyRandomizedTrees.jl
@@ -17,11 +17,13 @@ function ExtraTrees(data, labels; ntrees = 32, kargs...)
 	ExtraTrees([ExtremelyRandomizedTree(data, labels; kargs...) for i in 1:ntrees])
 end
 
-function ExtremelyRandomizedTree{T1,T2}(data::Array{T1,2}, labels::Array{T2,2}; kargs...)
+function ExtremelyRandomizedTree{T1<:Number,T2<:Number}(data::Array{T1,2}, labels::Array{T2,2}; kargs...)
 
 	assert(!isempty(data))
 	assert(!isempty(labels))
 	assert(size(data,2) == size(labels,2))
+	assert(!any(isnan(data)))
+	assert(!any(isnan(data)))
 
 	ExtremelyRandomizedTree(buildSingleTree(data, labels; kargs...)...)
 end
@@ -56,7 +58,7 @@ tree = ones(5, initalSize)
     function buildTree(data, labels, depth)
         # 	for all features
         # 		all data constant?
-        # 		store indices to nonconstantFeatures
+        # 		store indices to nonConstantFeatures
         #
         # length(data) < nMin ?
         #
@@ -77,7 +79,7 @@ tree = ones(5, initalSize)
             makeLeaf = !any(labels != labels[1])
         end
         if !makeLeaf
-            nonConstantFeatures = Array(Bool,size(data,1))
+			nonConstantFeatures = falses(size(data,1))
             for featureInd = 1:size(data,1)
                 firstDatum = data[featureInd,1]
                 for i = 2:size(data,2)
@@ -112,13 +114,13 @@ tree = ones(5, initalSize)
 				for i = 1:nclasses
 					classFrequencies[i] = sum(labels .== i)
 				end
-				leafs[:,nLeafs] = classFrequencies
+				leafs[:,nLeafs] = classFrequencies./sum(classFrequencies)
 			end
            
             tree[3:4,nodeInd] = nodeInd
             tree[5,nodeInd] = nLeafs
         else
-            # 1) select random K from nonconstantFeatures
+            # 1) select random K from nonConstantFeatures
             nonConstantFeatures = find(nonConstantFeatures)
             randIndices = randperm(length(nonConstantFeatures)) 
             selectedFeatures = nonConstantFeatures[
@@ -139,7 +141,7 @@ tree = ones(5, initalSize)
                 scores = zeros(size(splits))
                 for splitInd = 1:length(splits)
                     scores(splitInd) = 
-                        computeScore(data(selectedFeatures(splitInd),:), labels, splits(splitInd), nclasses)
+                        computeScore(regression, data(selectedFeatures(splitInd),:), labels, splits(splitInd), nclasses)
                 end
                 bestSplitInd = indmax(scores)
             else
@@ -152,7 +154,7 @@ tree = ones(5, initalSize)
             # 4) split data according to split s*, d1, d2
             dataind = 
                 vec(data[selectedFeatures[bestSplitInd],:] .< splits[bestSplitInd])
-            
+
             # 5) for both parts: t1=buildTree(d1), t2=buildTree(d2)
             maxdepth = max(depth,maxdepth)
             
@@ -170,7 +172,7 @@ end
  
 
 
-function predict{T}(a::ExtraTrees, data::Array{T,2}; votesfor = 1:size(a.trees[1].leafs,1))
+function predict{T<:Number}(a::ExtraTrees, data::Array{T,2}; votesfor = 1:size(a.trees[1].leafs,1))
 	if isempty(data) 
 		result = []
 		votes = []
@@ -208,16 +210,14 @@ function predict{T}(a::ExtraTrees, data::Array{T,2}; votesfor = 1:size(a.trees[1
 		final[todo] = nodeInds
 		# @show tree final tree[5,final] extratree.leafs
 		votes = votes + extratree.leafs[votesfor, vec(tree[5,final])]
-		#assert(isequal(votes,votes2))
 	end
 
 	if a.trees[1].regression
 		votes./length(a.trees)
 	else
 		if length(votesfor) != size(a.trees[1].leafs,1)
-			votes./sum(votes,2)
+			votes
 		else
-			votes = votes./sum(votes,1)
 			result = zeros(1, size(data,2))
 			for i = 1:size(data,2)
 				result[i] = indmax(votes[:,i])
@@ -267,54 +267,84 @@ function mutualInformation(x, y, nclasses)
 	I = sum(Is[!isnan(Is)])
 end
  
-function computeScore(featureData, labels, split, nclasses)
+function volume(a)
+    if size(a,2)<2
+        return 0
+    end
+    a .-= mean(a,2)
+    c = cov(a')
+    v, _ = eig(c)
+    prod(sqrt(max(0.001,v)))
+end
 
-	#Hc = entropy(p, labels)
-	#Hs = entropy(p, )
-	#I  = mutualInformation(p, labels, 1+(featureData<split))
 
-	x = labels
-	y = 1+(featureData<split)
-
-	px = zeros(nclasses)
-	py = zeros(nclasses)
-	pxy = zeros(nclasses)
-	for i = 1:length(x)
-	    pxy[x[i],y[i]] = pxy[x[i],y[i]] + 1
-	    px[x[i]] = px[x[i]] + 1
-	    py[y[i]] = py[y[i]] + 1
-	end
-	#[px,py,pxy] = quickhist(nclasses, single(x), single(y))
-
-	#assert(isequal(px,px2))
-	#assert(isequal(py,py2))
-	#assert(isequal(pxy,pxy2))
-
-	px = px/length(x)
-	py = py/length(x)
-	pxy = pxy/length(x)
-
-	hc = zeros(size(px))
-	hs = zeros(1,2)
-	for i = 1:length(px)
-		hc[i] = px[i]*log(px[i])
-	end
-	for i = 1:2
-		hs[i] = py[i]*log(py[i])
-	end
-	Hc = - sum(hc)
-	Hs = - sum(hs)
-
-	Is = zeros(nclasses,2)
-	for xi = 1:nclasses
-		for yi = 1:2
-			Is[xi,yi] = pxy[xi,yi]*log( pxy[xi,yi]/(px[xi]*py[yi]))
+function computeScore(regression, featureData, labels, split, nclasses)
+	if regression
+		ind = vec(featureData .< split)
+		n = length(ind)
+		nleft = sum(ind)
+		nright = n-nleft
+		assert(nleft>0)
+		assert(nright>0)
+		if size(labels,1) == 1
+ 			v = var(labels)
+			vleft = var(labels[ind])
+			vright = var(labels[!ind])
+		else
+			v = volume(labels)
+ 			vleft = volume(labels[:,ind])
+			vright = volume(labels[:,!ind])
 		end
-	end
-	Is = vec(Is)
-	I = sum(Is[!isnan(Is)])
+		assert(v)>0
+		score = (v - nleft/n*vleft - nright/n*vright)/v
+	else
+		#Hc = entropy(p, labels)
+		#Hs = entropy(p, )
+		#I  = mutualInformation(p, labels, 1+(featureData<split))
 
-	score = 2*I/(Hc + Hs)
+		x = labels
+		y = 1+(featureData<split)
+
+		px = zeros(nclasses)
+		py = zeros(nclasses)
+		pxy = zeros(nclasses)
+		for i = 1:length(x)
+			pxy[x[i],y[i]] = pxy[x[i],y[i]] + 1
+			px[x[i]] = px[x[i]] + 1
+			py[y[i]] = py[y[i]] + 1
+		end
+		#[px,py,pxy] = quickhist(nclasses, single(x), single(y))
+
+		#assert(isequal(px,px2))
+		#assert(isequal(py,py2))
+		#assert(isequal(pxy,pxy2))
+
+		px = px/length(x)
+		py = py/length(x)
+		pxy = pxy/length(x)
+
+		hc = zeros(size(px))
+		hs = zeros(1,2)
+		for i = 1:length(px)
+			hc[i] = px[i]*log(px[i])
+		end
+		for i = 1:2
+			hs[i] = py[i]*log(py[i])
+		end
+		Hc = - sum(hc)
+		Hs = - sum(hs)
+
+		Is = zeros(nclasses,2)
+		for xi = 1:nclasses
+			for yi = 1:2
+				Is[xi,yi] = pxy[xi,yi]*log( pxy[xi,yi]/(px[xi]*py[yi]))
+			end
+		end
+		Is = vec(Is)
+		I = sum(Is[!isnan(Is)])
+
+		score = 2*I/(Hc + Hs)
+	end
 end
 
 
